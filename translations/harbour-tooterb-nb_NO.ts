7125f6ca00d2b3ef7be4dca9e6ba18ca8f28de18
Mark Washeim <blueprint@poetaster.de>
2026-01-03 19:58:52 +0100

    Duplicate harbour-tooterb-no_NB.ts history in harbour-tooterb-nb_NO.ts history.

---
+++ translations/harbour-tooterb-nb_NO.ts
@@ -0,0 +1,336 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="NO_nb">
+<context>
+    <name>API</name>
+    <message>
+        <source>favourited</source>
+        <translation>favorisert</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>følger deg</translation>
+    </message>
+    <message>
+        <source>boosted</source>
+        <translation>boostet</translation>
+    </message>
+    <message>
+        <source>said</source>
+        <translation>sa</translation>
+    </message>
+</context>
+<context>
+    <name>Conversation</name>
+    <message>
+        <source>Conversation</source>
+        <translation>Samtale</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Slette</translation>
+    </message>
+    <message>
+        <source>Emojis</source>
+        <translation>Emojis</translation>
+    </message>
+    <message>
+        <source>Tap to insert</source>
+        <translation>Trykk for å sette inn</translation>
+    </message>
+    <message>
+        <source>Write your warning here</source>
+        <translation>Skriv advarselen din her</translation>
+    </message>
+    <message>
+        <source>Public</source>
+        <translation>Offentlig</translation>
+    </message>
+    <message>
+        <source>Unlisted</source>
+        <translation>Ikke oppført</translation>
+    </message>
+    <message>
+        <source>Followers-only</source>
+        <translation>Kun for følgere</translation>
+    </message>
+    <message>
+        <source>Direct</source>
+        <translation>Direkte</translation>
+    </message>
+    <message>
+        <source>What&apos;s on your mind?</source>
+        <translation>Hva tenker du på?</translation>
+    </message>
+</context>
+<context>
+    <name>ImageFullScreen</name>
+    <message>
+        <source>Error loading</source>
+        <translation>Feil ved lasting</translation>
+    </message>
+</context>
+<context>
+    <name>ImageUploader</name>
+    <message>
+        <source>The file %1 does not exists</source>
+        <translation>Filen %1 finnes ikke</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation>Logg inn</translation>
+    </message>
+    <message>
+        <source>Instance</source>
+        <translation>Instans</translation>
+    </message>
+    <message>
+        <source>Enter a valid Mastodon instance URL</source>
+        <translation>Fyll i URL till Mastodoninstans</translation>
+    </message>
+    <message>
+        <source>Mastodon is a free, open-source social network. A decentralized alternative to commercial platforms, it avoids the risks of a single company monopolizing your communication. Pick a server that you trust — whichever you choose, you can interact with everyone else. Anyone can run their own Mastodon instance and participate in the social network seamlessly.</source>
+        <translation>Mastodon er et gratis sosialt nettverk med åpen kildekode. Som et desentralisert alternativ til kommersielle plattformer unngår det risikoen for at et enkelt selskap monopoliserer kommunikasjonen din. Velg en server du stoler på – uansett hvilken du velger, kan du samhandle med alle andre. Hvem som helst kan kjøre sin egen Mastodon-instans og sømløst delta i det sosiale nettverket.</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Last på nytt</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Home</source>
+        <translation>Hjem</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Varslinger</translation>
+    </message>
+    <message>
+        <source>New Toot</source>
+        <translation>Ny toot</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Søk</translation>
+    </message>
+    <message>
+        <source>@user or #term</source>
+        <translation>@bruker eller #emne</translation>
+    </message>
+    <message>
+        <source>Local</source>
+        <translation>Lokalt</translation>
+    </message>
+    <message>
+        <source>Federated</source>
+        <translation>Federert</translation>
+    </message>
+</context>
+<context>
+    <name>MiniStatus</name>
+    <message>
+        <source>boosted</source>
+        <translation>boostet</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>favorisert</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>følger deg</translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Load more</source>
+        <translation>Last inn mer</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instillinger</translation>
+    </message>
+    <message>
+        <source>Loading</source>
+        <translation>Laster inn</translation>
+    </message>
+    <message>
+        <source>please wait...</source>
+        <translation>vennligst vent...</translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Unfollow</source>
+        <translation>Slutte å følge</translation>
+    </message>
+    <message>
+        <source>Follow request sent!</source>
+        <translation>Följarförfrågan har skickats!</translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <translation>Følger</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Demp</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation>Fjern demping</translation>
+    </message>
+    <message>
+        <source>Unblock</source>
+        <translation>Avblokker</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation>Blokker</translation>
+    </message>
+    <message>
+        <source>Statuses</source>
+        <translation>Statuser</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favoritter</translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <translation>Følg</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation>Oversikt</translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <translation>Følgere</translation>
+    </message>
+    <message>
+        <source>Bio</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Open Profile in Browser</source>
+        <translation>Åpne profil i nettleser</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Instillinger</translation>
+    </message>
+    <message>
+        <source>Remove Account</source>
+        <translation>Fjern konto</translation>
+    </message>
+    <message>
+        <source>Add Account</source>
+        <translation>Legg til konto</translation>
+    </message>
+    <message>
+        <source>Deauthorize this app and remove your account</source>
+        <translation>Fjern autorisasjonen til denne appen og fjern kontoen din</translation>
+    </message>
+    <message>
+        <source>Authorize this app to access your Mastodon account</source>
+        <translation>Godkjenn denne appen for å få tilgang til Mastodon-kontoen din</translation>
+    </message>
+    <message>
+        <source>Disable this option if you want to preserve your data connection</source>
+        <translation>Deaktiver dette alternativet hvis du vil bevare datatilkoblingen din</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Takk til</translation>
+    </message>
+    <message>
+        <source>UI/UX design and development</source>
+        <translation>UI/UX-design og -utvikling</translation>
+    </message>
+    <message>
+        <source>Visual identity</source>
+        <translation>Visuell identitet</translation>
+    </message>
+    <message>
+        <source>Occitan &amp; French translation</source>
+        <translation>Fransk oversettelse</translation>
+    </message>
+    <message>
+        <source>Dutch translation</source>
+        <translation>Nederlandsk oversettelse</translation>
+    </message>
+    <message>
+        <source>Spanish translation</source>
+        <translation>Spansk oversettelse</translation>
+    </message>
+    <message>
+        <source>Added README file</source>
+        <translation>Publiser README-fil</translation>
+    </message>
+    <message>
+        <source>Chinese translation</source>
+        <translation>Kinesisk oversettelse</translation>
+    </message>
+    <message>
+        <source>Load images in toots</source>
+        <translation>Last inn bilder i toots</translation>
+    </message>
+    <message>
+        <source>Translate</source>
+        <translation>Oversett</translation>
+    </message>
+    <message>
+        <source>Use Transifex to help with app translation to your language</source>
+        <translation>Bruk Transifex for å oversette appen til språket ditt</translation>
+    </message>
+    <message>
+        <source>Development and translations</source>
+        <translation>Utvikling og oversettelser/translation>
+    </message>
+</context>
+<context>
+    <name>Toot</name>
+    <message>
+        <source>boosted</source>
+        <translation>boostet</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>favorisert</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>følger deg</translation>
+    </message>
+</context>
+<context>
+    <name>VisualContainer</name>
+    <message>
+        <source>Unboost</source>
+        <translation>Fjern boost</translation>
+    </message>
+    <message>
+        <source>Boost</source>
+        <translation>Boost</translation>
+    </message>
+    <message>
+        <source>Unfavorite</source>
+        <translation>Avfavorisere</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Favorisere</translation>
+    </message>
+</context>
+</TS>

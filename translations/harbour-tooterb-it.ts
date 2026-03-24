45fd7a504744bd336dfd7cf356cdba8c5530a4ca
legacychimera247 <mad_fer_it@vivaldi.net>
2026-03-24 15:59:57 +0100

    Add files via upload

---
+++ translations/harbour-tooterb-it.ts
@@ -0,0 +1,549 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>API</name>
+    <message>
+        <source>favourited</source>
+        <translation>ha apprezzato</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>ti sta seguendo</translation>
+    </message>
+    <message>
+        <source>boosted</source>
+        <translation>ha condiviso</translation>
+    </message>
+    <message>
+        <source>said</source>
+        <translation>ha detto</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationPage</name>
+    <message>
+        <source>Copy Link to Clipboard</source>
+        <extracomment>Use the translation of &quot;Copy Link&quot; for a shorter PullDownMenu label</extracomment>
+        <translation>Copia link</translation>
+    </message>
+    <message>
+        <source>Write your warning here</source>
+        <extracomment>placeholderText in Toot content warning panel</extracomment>
+        <translation>Contenuto avviso</translation>
+    </message>
+    <message>
+        <source>What&apos;s on your mind?</source>
+        <extracomment>placeholderText in Toot text panel</extracomment>
+        <translation>A cosa stai pensando?</translation>
+    </message>
+    <message>
+        <source>Toot sent!</source>
+        <translation>Toot pubblicato!</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Apri nel browser</translation>
+    </message>
+    <message>
+        <source>Edit Alt Text</source>
+        <translation>Modifica testo alternativo</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Toot edited!</source>
+        <translation>Toot modificato!</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+    <message>
+        <source>Describe this media for visually impaired users</source>
+        <translation>Descivi questo contenuto per gli utenti con problemi di vista</translation>
+    </message>
+    <message>
+        <source>Alt text helps make content accessible to people who are blind or have low vision.</source>
+        <translation>Il testo alternativo aiuta a rendere il testo accessibile a chi è cieco o vede poco.</translation>
+    </message>
+    <message>
+        <source>Quoting @%1</source>
+        <translation>Citando @%1</translation>
+    </message>
+    <message>
+        <source>Removing</source>
+        <translation>Sto rimuovendo</translation>
+    </message>
+    <message>
+        <source>Public</source>
+        <translation>Pubblico</translation>
+    </message>
+    <message>
+        <source>Unlisted</source>
+        <translation>Non in lista</translation>
+    </message>
+    <message>
+        <source>Followers-only</source>
+        <translation>Solo per i follower</translation>
+    </message>
+    <message>
+        <source>Direct</source>
+        <translation>Messaggio diretto</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>New Toot</source>
+        <translation>Nuovo Toot</translation>
+    </message>
+</context>
+<context>
+    <name>EmojiSelect</name>
+    <message>
+        <source>Emojis</source>
+        <translation>Emojis</translation>
+    </message>
+    <message>
+        <source>Tap to insert</source>
+        <translation>Tap per inserire</translation>
+    </message>
+</context>
+<context>
+    <name>ImageUploader</name>
+    <message>
+        <source>The file %1 does not exists</source>
+        <translation>Il file %1 non esiste</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation>Accesso</translation>
+    </message>
+    <message>
+        <source>Instance</source>
+        <translation>Istanza</translation>
+    </message>
+    <message>
+        <source>Mastodon is a free, open-source social network. A decentralized alternative to commercial platforms, it avoids the risks of a single company monopolizing your communication. Pick a server that you trust — whichever you choose, you can interact with everyone else. Anyone can run their own Mastodon instance and participate in the social network seamlessly.</source>
+        <translation>Mastodon è un servizio di rete sociale in software libero, costituito in una federazione d&apos;istanze. Mastodon fa parte del più ampio Fediverso, permettendo ai suoi utenti di interagire anche con utenti su diverse piattaforme aperte che supportano lo stesso protocollo.</translation>
+    </message>
+    <message>
+        <source>Enter a valid Mastodon instance URL (will open a web browser for Authentication)</source>
+        <translation>Inserisci un URL valido per un&apos;instanza di Mastodon (questo aprirà un browser web per L&apos;autenticazione)</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Scrivi</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifiche</translation>
+    </message>
+    <message>
+        <source>Local</source>
+        <translation>Locale</translation>
+    </message>
+    <message>
+        <source>Federated</source>
+        <translation>Federato</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+    <message>
+        <source>@user or #term</source>
+        <translation>@utente o #hashtag</translation>
+    </message>
+    <message>
+        <source>New Toot</source>
+        <translation>Nuovo Toot</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Segnalibro</translation>
+    </message>
+    <message>
+        <source>Press and hold the home tab to switch account</source>
+        <translation>Premi e tieni premuto il pulsante Home per cambiare account al volo</translation>
+    </message>
+    <message>
+        <source>Trending</source>
+        <translation>Di tendenza</translation>
+    </message>
+</context>
+<context>
+    <name>MediaFullScreen</name>
+    <message>
+        <source>Error loading</source>
+        <translation>Errore durante il caricamento</translation>
+    </message>
+</context>
+<context>
+    <name>MediaItem</name>
+    <message>
+        <source>Image</source>
+        <translation>Immagine</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <source>PDF document</source>
+        <translation>Documento PDF</translation>
+    </message>
+</context>
+<context>
+    <name>MiniStatus</name>
+    <message>
+        <source>boosted</source>
+        <translation>ha condiviso</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>ha apprezzato</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>ti sta seguendo</translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>New Toot</source>
+        <translation>Nuovo Toot</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Apri nel browser</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message>
+        <source>Nothing found</source>
+        <translation>Non ho trovato nulla</translation>
+    </message>
+    <message>
+        <source>My Profile</source>
+        <translation>Profilo personale</translation>
+    </message>
+</context>
+<context>
+    <name>ProfileHeader</name>
+    <message>
+        <source>Bot</source>
+        <translation>Bot</translation>
+    </message>
+    <message>
+        <source>Follows you</source>
+        <translation>Ti segue</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Gruppo</translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePage</name>
+    <message>
+        <source>About</source>
+        <extracomment>If there&apos;s no good translation for &quot;About&quot;, use &quot;Details&quot; (in details about profile).</extracomment>
+        <translation>Dettagli</translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <extracomment>Will show as: &quot;35 Followers&quot;</extracomment>
+        <translation>Seguaci</translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <extracomment>Will show as: &quot;23 Following&quot;</extracomment>
+        <translation>Segue</translation>
+    </message>
+    <message>
+        <source>Statuses</source>
+        <extracomment>Will show as: &quot;115 Statuses&quot;</extracomment>
+        <translation>Toots</translation>
+    </message>
+    <message>
+        <source>Mention</source>
+        <translation>Menziona</translation>
+    </message>
+    <message>
+        <source>Unfollow</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Smetti di seguire</translation>
+    </message>
+    <message>
+        <source>Requested</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Richiesto</translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Segui</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Non silenziare</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Silenzia</translation>
+    </message>
+    <message>
+        <source>Unblock</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Sblocca</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <extracomment>Is a button. Keep it as short as possible.</extracomment>
+        <translation>Blocca</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opzioni</translation>
+    </message>
+    <message>
+        <source>Load Images in Toots</source>
+        <translation>Carica immagini nei Toots</translation>
+    </message>
+    <message>
+        <source>Disable this option if you want to preserve your data connection</source>
+        <translation>Disabilitare questa opzione per conservare connessione dati</translation>
+    </message>
+    <message>
+        <source>Account</source>
+        <translation>Account</translation>
+    </message>
+    <message>
+        <source>Remove Account</source>
+        <translation>Rimozione del account</translation>
+    </message>
+    <message>
+        <source>Add Account</source>
+        <translation>Aggiungi account</translation>
+    </message>
+    <message>
+        <source>Deauthorize this app from using your account and remove account data from phone</source>
+        <translation>Annullare l&apos;autorizzazione dell&apos;app e rimuovere l&apos;account</translation>
+    </message>
+    <message>
+        <source>Authorize this app to access your Mastodon account</source>
+        <translation>Autorizzare l&apos;app all&apos;utilizzo del conto Mastodon</translation>
+    </message>
+    <message>
+        <source>Translate</source>
+        <translation>Tradurre</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <extracomment>Translation alternative: &quot;Development&quot;</extracomment>
+        <translation>Sviluppo</translation>
+    </message>
+    <message>
+        <source>UI/UX design and development</source>
+        <translation>Design UI/UX e sviluppo</translation>
+    </message>
+    <message>
+        <source>Visual identity</source>
+        <translation>Identità visiva</translation>
+    </message>
+    <message>
+        <source>Development and translations</source>
+        <translation>Sviluppo e traduzioni</translation>
+    </message>
+    <message>
+        <source>Occitan &amp; French translation</source>
+        <translation>Traduzione francese e occitanica</translation>
+    </message>
+    <message>
+        <source>Chinese translation</source>
+        <translation>Traduzione cinese</translation>
+    </message>
+    <message>
+        <source>Dutch translation</source>
+        <translation>Traduzione olandese</translation>
+    </message>
+    <message>
+        <source>Spanish translation</source>
+        <translation>Traduzione spagnola</translation>
+    </message>
+    <message>
+        <source>Use</source>
+        <extracomment>Full sentence for translation: &quot;Use Transifex to help with app translation to your language.&quot; - The word Transifex is a link and doesn&apos;t need translation.</extracomment>
+        <translation>Utilizzare</translation>
+    </message>
+    <message>
+        <source>to help with app translation to your language.</source>
+        <translation>per aiutare nella traduzione dell&apos;app.</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>Sviluppo</translation>
+    </message>
+    <message>
+        <source>Documentation</source>
+        <translation>Documentazione</translation>
+    </message>
+    <message>
+        <source>Mention</source>
+        <translation>Menzionare</translation>
+    </message>
+    <message>
+        <source>Account removed</source>
+        <translation>Account rimosso</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Norwegian Translation</source>
+        <translation>Traduzione norvegese</translation>
+    </message>
+    <message>
+        <source>Font Size</source>
+        <translation>Dimensione carattere</translation>
+    </message>
+    <message>
+        <source>Sample text</source>
+        <translation>Testo di prova</translation>
+    </message>
+    <message>
+        <source>Quick Scroll Arrows</source>
+        <translation>Freccie Scorrimento Veloce</translation>
+    </message>
+    <message>
+        <source>Show arrows to jump to top/bottom when scrolling fast</source>
+        <translation>Mostra freccie per saltare in alto/basso quando scorri velocemente</translation>
+    </message>
+</context>
+<context>
+    <name>VisualContainer</name>
+    <message>
+        <source>Show less</source>
+        <translation>Mostra meno</translation>
+    </message>
+    <message>
+        <source>Show more</source>
+        <translation>Mostra di più</translation>
+    </message>
+    <message>
+        <source>Conversation</source>
+        <translation>Conversazione</translation>
+    </message>
+    <message>
+        <source>Unfavorite</source>
+        <translation>Rimuovi dai preferiti</translation>
+    </message>
+    <message>
+        <source>Favorite</source>
+        <translation>Aggiungi ai preferiti</translation>
+    </message>
+    <message>
+        <source>Unboost</source>
+        <translation>Non condividere più</translation>
+    </message>
+    <message>
+        <source>Boost</source>
+        <translation>Condividi</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Rispondi</translation>
+    </message>
+    <message>
+        <source>Quote</source>
+        <translation>Cita</translation>
+    </message>
+    <message>
+        <source>Remove Bookmark</source>
+        <translation>Rimuovi Preferito</translation>
+    </message>
+    <message>
+        <source>Bookmark</source>
+        <translation>Aggiungi Preferito</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Elimina</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Sto eliminando</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Modifica</translation>
+    </message>
+    <message>
+        <source>Mention</source>
+        <translation>Menziona</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>Caricamento...</translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation>Mostra altro</translation>
+    </message>
+    <message>
+        <source>Vote</source>
+        <translation>Vota</translation>
+    </message>
+    <message>
+        <source>votes</source>
+        <translation>voti</translation>
+    </message>
+    <message>
+        <source>Closed</source>
+        <translation>Chiuso</translation>
+    </message>
+    <message>
+        <source>days left</source>
+        <translation>giorni rimasti</translation>
+    </message>
+    <message>
+        <source>hours left</source>
+        <translation>ore rimaste</translation>
+    </message>
+    <message>
+        <source>minutes left</source>
+        <translation>minuti rimasti</translation>
+    </message>
+</context>
+</TS>
